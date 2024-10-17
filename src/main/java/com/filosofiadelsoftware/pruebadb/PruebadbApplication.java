e8a30b3ab5190115ca364a11a3c7168dc52c5384
Daniel Saavedra <daniel.saavedra.fon@gmail.com>
2024-10-16 20:23:07 -0500

    Initial commit

---
+++ src/main/java/com/filosofiadelsoftware/pruebadb/PruebadbApplication.java
@@ -0,0 +1,13 @@
+package com.filosofiadelsoftware.pruebadb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebadbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebadbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filosofiadelsoftware/pruebadb/PruebadbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filosofiadelsoftware.pruebadb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebadbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
